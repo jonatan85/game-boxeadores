b10c1393afc59f58f30370e080d93cb718e5a49a
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2024-02-21 00:03:12 +0100

    segundo

--- src/app/pages/boxeadores/boxeadores.page.ts
+++ src/app/pages/boxeadores/boxeadores.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Boxeadores } from 'src/app/servicios/boxeadores/boxeadores.models';
 import { BoxeadoresService } from 'src/app/servicios/boxeadores/boxeadores.service';
-
+import { BoxeadoresRingService } from 'src/app/servicios/boxeadoresRing/boxeadores-ring.service';
 @Component({
   selector: 'app-boxeadores',
   templateUrl: './boxeadores.page.html',
@@ -11,10 +11,12 @@ import { BoxeadoresService } from 'src/app/servicios/boxeadores/boxeadores.servi
 export class BoxeadoresPage implements OnInit {
   public boxeadores?: Observable<Boxeadores[]>
   public boxeador: Boxeadores[] = [];
+  public selectBoxeadores: Boxeadores | null = null;
  
 
   constructor(
-    private boxeadoresService: BoxeadoresService
+    private boxeadoresService: BoxeadoresService,
+    private boxeadorRingService: BoxeadoresRingService
   ) { }
 
   ngOnInit() {
@@ -22,11 +24,26 @@ export class BoxeadoresPage implements OnInit {
   }
 
   public seleccionaJugador(_id:string) {
-    console.log(_id);
-    
+    this.boxeadoresService.getBoxeadores().subscribe(boxeadores => {
+      const selectBoxeador1 = boxeadores.find(boxeador => boxeador._id === _id);
+      this.selectBoxeadores = selectBoxeador1 || null;
+      console.log('Boxeador selccionado', this.selectBoxeadores);
+      
+    })
+    const boxeador2 = this.selecccionarJugadorAlAzar()
+  }
+
+  public selecccionarJugadorAlAzar() {
+    this.boxeadoresService.getBoxeadores().subscribe(boxeadores => {
+      this.boxeadorRingService.selectBoxeadoresRandom(boxeadores).subscribe(randomBoxeador => {
+        this.selectBoxeadores = randomBoxeador;
+        console.log('Boxeador seleccionado al azar', this.selectBoxeadores);
+        
+      })
+    });
   }
 
 
 
-
 }
+
--- src/app/servicios/boxeadoresRing/boxeadores-ring.service.ts
+++ src/app/servicios/boxeadoresRing/boxeadores-ring.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Boxeadores } from '../boxeadores/boxeadores.models';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,14 @@ import { Injectable } from '@angular/core';
 export class BoxeadoresRingService {
 
   constructor() { }
+
+  public selectBoxeadoresRandom (boxeadores: Boxeadores[]): Observable<Boxeadores | null>{
+    if (boxeadores && boxeadores.length > 0 ) {
+      const randomIndex = Math.floor(Math.random() * boxeadores.length );
+      const randomBoxeador = boxeadores[randomIndex];
+      return of(randomBoxeador);
+    } else {
+      return of(null);
+    }
+  }
 }
